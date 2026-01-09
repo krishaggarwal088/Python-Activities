48e45efb8a1296227bb06cef256037b7019fa824
krishaggarwal088 <krishagg088@gmail.com>
2026-01-09 21:43:10 +0530

    string

---
+++ lesson 1/activity3.py
@@ -0,0 +1,15 @@
+first_name = "Krish"
+last_name = "Aggarwal"
+full_name = first_name+last_name
+example = "Meow"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = 'Mrinank'
+print("Length of String:", len (word))
+print("First Letter of String:", word[0])
+print("Last Letter of String:", word [7])
+print("String Sliced", word [0:3])
