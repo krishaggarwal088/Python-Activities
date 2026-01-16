4f35615231ac562e67cc8485e97dfdd7a03d1cb7
krishaggarwal088 <krishagg088@gmail.com>
2026-01-16 21:36:47 +0530

    sum

---
+++ lesson 3/activity3.py
@@ -0,0 +1,4 @@
+# Python program to calculate the sum of the first sixty seven natural numbers
+# Use built-in sum and range functions to calculate the sum
+total_sum = sum(range(1, 67))
+print(f"The sum of the first sixty six natural numbers is {total_sum}")
